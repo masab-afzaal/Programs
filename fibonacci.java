e4b53ad692730c9a27157144ba5abe3b3f16ee63
masab-afzaal <110492975+masab-afzaal@users.noreply.github.com>
2022-10-25 15:22:44 +0500

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,12 +1,22 @@
+import java.util.*;
 public class fibonacci {
-	public static void main(String[] args){
-		int n1=0,n2=1,n3;
-		for(int i = 2; i<20;i++)
-		{
-		 n3 = n1 + n2;
-		 System.out.print(" "+n3);
-		 n1=n2;
-		 n2=n3;
-		}
-	}
+    public static void main ( String args [] ) {
+
+        System.out.print("enter nth times number : ");
+        
+        Scanner sc = new Scanner ( System.in);
+        int n = sc.nextInt();
+        int n1=0,n2=1,n3;
+        System.out.print("0 1");        
+        for ( int i = 3; i <= n; i++) {
+            n3=n1+n2;
+            
+            System.out.print(" " + n3);
+            
+            n1=n2;
+            n2=n3;
+        }
+
+    }
+    
 }
